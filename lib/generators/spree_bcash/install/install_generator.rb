26c2310259a43afe8c6f5b6cc3d20716c4a9c92e
Matheus Caceres <matheus@formaweb.com.br>
2013-04-20 16:58:52 -0300

    Rake migrations

---
+++ lib/generators/spree_bcash/install/install_generator.rb
@@ -0,0 +1,21 @@
+module Bcash
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+
+      def add_migrations
+        run 'bundle exec rake railties:install:migrations FROM=spree_bcash'
+      end
+
+      def run_migrations
+        question = ask "Would you like to run the migrations now? [Y/n]"
+
+        if question == "" || question.downcase == "y"
+          run 'bundle exec rake db:migrate'
+        else
+          puts "Skiping rake db:migrate, don't forget to run it!"
+        end
+      end
+
+    end
+  end
+end
