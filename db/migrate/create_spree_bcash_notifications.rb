fca795b4d634ab3a48ccced8d31700ea24beca98
Matheus Caceres <matheus@formaweb.com.br>
2013-04-20 17:04:50 -0300

    Create migrations

---
+++ db/migrate/create_spree_bcash_notifications.rb
@@ -0,0 +1,12 @@
+class CreateSpreeBcashNotifications < ActiveRecord::Migration
+  def change
+    create_table :spree_bcash_notification do |t|
+      t.integer :order_id
+      t.integer :id_transaction
+      t.string :status
+      
+      t.timestamps
+      
+    end
+  end
+end
